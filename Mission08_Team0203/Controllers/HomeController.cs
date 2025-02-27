d165fbb6f5c8baab7caa5b08855a176f21e3e240
JAKE HOOPES <hoopesjake@gmail.com>
2025-02-26 17:16:55 -0700

    Done. With Repo Pattern

--- Mission08_Team0203/Controllers/HomeController.cs
+++ Mission08_Team0203/Controllers/HomeController.cs
@@ -9,96 +9,89 @@ namespace Mission08_Team0203.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly TaskContext _context;
+        private readonly ITaskRepository _context;
 
-        public HomeController(TaskContext context)
+        public HomeController(ITaskRepository repo)
         {
-            _context = context;
+            _context = repo;
         }
 
         // Display Quadrants View
-        public async Task<IActionResult> Quadrant()
+        public IActionResult Quadrant()
         {
-            var tasks = await _context.Tasks.Include(t => t.Category).ToListAsync();
+            var tasks = _context.Tasks.Include(t => t.Category).ToList();
             return View(tasks);
         }
 
         // GET: Add Task View
-        public async Task<IActionResult> Add()
+        public IActionResult Add()
         {
-            ViewBag.Categories = new SelectList(await _context.Categories.ToListAsync(), "CategoryId", "CategoryName");
+            ViewBag.Categories = new SelectList(_context.Categories.ToList(), "CategoryId", "CategoryName");
             return View();
         }
 
         // POST: Add Task
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Add(TaskModel task)
+        public IActionResult Add(TaskModel task)
         {
             if (ModelState.IsValid)
             {
-                _context.Tasks.Add(task);
-                await _context.SaveChangesAsync();
+                _context.AddTask(task);
                 return RedirectToAction("Quadrant");
             }
 
             // Reload categories if ModelState is invalid
-            ViewBag.Categories = new SelectList(await _context.Categories.ToListAsync(), "CategoryId", "CategoryName");
+            ViewBag.Categories = new SelectList(_context.Categories.ToList(), "CategoryId", "CategoryName");
             return View(task);
         }
 
         // POST: Mark Task as Complete
         [HttpPost]
-        public async Task<IActionResult> MarkComplete(int id)
+        public IActionResult MarkComplete(int id)
         {
-            var task = await _context.Tasks.FindAsync(id);
+            var task = _context.Tasks.FirstOrDefault(t => t.TaskId == id);
             if (task != null)
             {
                 task.Completed = true;
-                await _context.SaveChangesAsync();
+                _context.UpdateTask(task);
             }
             return RedirectToAction("Quadrant");
         }
 
         // POST: Delete Task
         [HttpPost]
-        public async Task<IActionResult> Delete(int id)
+        public IActionResult Delete(int id)
         {
-            var task = await _context.Tasks.FindAsync(id);
-            if (task != null)
-            {
-                _context.Tasks.Remove(task);
-                await _context.SaveChangesAsync();
-            }
+            _context.DeleteTask(id);
             return RedirectToAction("Quadrant");
         }
 
         // GET: Edit Task View
-        public async Task<IActionResult> Edit(int id)
+        public IActionResult Edit(int id)
         {
-            var task = await _context.Tasks.FindAsync(id);
+            var task = _context.Tasks.FirstOrDefault(t => t.TaskId == id);
             if (task == null)
             {
                 return NotFound();
             }
 
-            ViewBag.Categories = new SelectList(await _context.Categories.ToListAsync(), "CategoryId", "CategoryName");
+            ViewBag.Categories = new SelectList(_context.Categories.ToList(), "CategoryId", "CategoryName");
             return View(task);
         }
 
         // POST: Edit Task
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(TaskModel task)
+        public IActionResult Edit(TaskModel task)
         {
             if (ModelState.IsValid)
             {
-                _context.Tasks.Update(task);
-                await _context.SaveChangesAsync();
+                _context.UpdateTask(task);
                 return RedirectToAction("Quadrant");
             }
 
-            ViewBag.Categories = new SelectList(await _context.Categories.ToListAsync(), "CategoryId", "CategoryName");
+            ViewBag.Categories = new SelectList(_context.Categories.ToList(), "CategoryId", "CategoryName");
             return View(task);
         }
     }
